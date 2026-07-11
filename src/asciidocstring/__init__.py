859a5030c40848cd8f4df01a9d88feacbbd23a50
Michael R. Bernstein <zopemaven@gmail.com>
2026-07-11 09:05:29 -0600

    chore: prepare version 0.1.0a1 and resolve PyPI content-type requirements

--- src/asciidocstring/__init__.py
+++ src/asciidocstring/__init__.py
@@ -1,5 +1,5 @@
 from .document import AsciiDocStringDocument, parse
 from .visitors import TestBlock
 
-__version__ = "0.1.0"
+__version__ = "0.1.0a1"
 __all__ = ["parse", "AsciiDocStringDocument", "TestBlock"]
